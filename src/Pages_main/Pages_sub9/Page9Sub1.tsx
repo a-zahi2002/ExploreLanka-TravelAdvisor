e82cfb58281e65a1609506dab083483343f1aa7e
FengZhi <zahi.mfa.2002@gmail.com>
2024-09-23 16:03:23 +0530

    scroll fix

--- src/Pages_main/Pages_sub9/Page9Sub1.tsx
+++ src/Pages_main/Pages_sub9/Page9Sub1.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import "../Pages_sub1.css";
+import "../Pages_sub1.css"; // Sub Page CSS
 
 const Page9Sub1: React.FC = () => {
   return (
     <div className="pages_sub1">
       <div className="content-container">
         <h1>Gangaramaya Temple</h1>
+        {/* Add a close button to navigate back to the main page */}
+        <button onClick={() => window.history.back()}>Close</button>
       </div>
     </div>
   );
