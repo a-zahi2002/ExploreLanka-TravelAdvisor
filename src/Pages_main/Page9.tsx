00b7058d9b4292eccfebc753592ebf56457e805d
nadun372 <liyanage2021@gmail.com>
2024-09-24 13:31:56 +0530

    css changed

--- src/Pages_main/Page9.tsx
+++ src/Pages_main/Page9.tsx
@@ -8,7 +8,12 @@ const Page9: React.FC = () => {
 
   return (
     <div className="page">
-      <h1>WESTERN PROVINCE</h1>
+      <h1 className="topic">Western Province-Sri Lanka</h1>
+      <p className='paragraph'>Have you visit the Beautiful Western Province?</p>
+      <p>The province has an area of 3,694.20 km2 and a population of 5,648,000. Major towns include Colombo,Kluthara and Gampaha. Colombo is the capital of the province.
+      </p>
+      
+
       <TileGrid9 />
       <button onClick={() => navigate(-1)}>Close</button> {/* Navigate Back */}
     </div>
