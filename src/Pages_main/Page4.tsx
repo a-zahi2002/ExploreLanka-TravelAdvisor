b560a8f1783439723e76057fe2986bfe127295be
FengZhi <zahi.mfa.2002@gmail.com>
2024-09-28 20:58:13 +0530

    page4 adjustment

--- src/Pages_main/Page4.tsx
+++ src/Pages_main/Page4.tsx
@@ -8,16 +8,16 @@ const Page4: React.FC = () => {
 
   return (
     <div className="page">
-      <h1 className="topic">Uva Province-Sri Lanka</h1>
+      <p className="topic">Uva Province-Sri Lanka</p>
       <p className="paragraph">
-      Discover the beauty of Uva Province, Sri Lanka
+        Discover the beauty of Uva Province, Sri Lanka
       </p>
       <p>
-      The Uva Province is one of the nine provinces of Sri Lanka. The province has an area of 8,500 km² and a 
-    population of 1,266,463, making it the 2nd least populated province. The provincial capital is Badulla. 
-    Uva is bordered by the Eastern, Southern, Sabaragamuwa, and Central provinces.
+        The Uva Province is one of the nine provinces of Sri Lanka. The province
+        has an area of 8,500 km² and a population of 1,266,463, making it the
+        2nd least populated province. The provincial capital is Badulla. Uva is
+        bordered by the Eastern, Southern, Sabaragamuwa, and Central provinces.
       </p>
-
       <TileGrid4 />
       <button onClick={() => navigate(-1)}>Back</button>{" "}
       {/* Better for navigating back */}
