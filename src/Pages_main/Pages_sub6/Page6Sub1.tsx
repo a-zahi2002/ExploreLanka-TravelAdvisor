9dc8d0a100cad93da0f9e0ad0c4dee5d652e3eca
Najla200 <najlahamza52@gmail.com>
2024-09-28 11:36:57 +0530

    Update Page6Sub1.tsx

--- src/Pages_main/Pages_sub6/Page6Sub1.tsx
+++ src/Pages_main/Pages_sub6/Page6Sub1.tsx
@@ -5,14 +5,7 @@ const Page6Sub1: React.FC = () => {
   return (
     <div className="pages_sub1">
       <div className="content-container">
-        <h1>Nine Arches Railway Bridge</h1>
-        <p>The Nine Arches Railway Bridge, also known as the Demodara Bridge, is an iconic structure in the Ella region of Sri Lanka. Built in the early 20th century, 
-          it features nine stunning stone arches that span a deep ravine, connecting the towns of Demodara and Ella.
-          This British colonial engineering marvel supports the railway line between Colombo and Badulla and is crafted from locally quarried stones that blend with the surrounding landscapes.
-          Set amidst lush tea plantations and forests, 
-          the bridge offers breathtaking views of the Ella Gap, attracting tourists, photographers, and railway enthusiasts.
-          Visitors often walk along the tracks to capture the scenic beauty of the bridge. Despite its age, it remains a vital transportation link in Sri Lanka’s hill country,
-          showcasing the enduring legacy of colonial engineering and the island's natural beauty.</p>
+        <h1>Page6 Sub1</h1>
       </div>
     </div>
   );
