5b16ac5ecd09583290867ce8b7c86b5682fbd500
Najla200 <najlahamza52@gmail.com>
2024-09-28 11:38:42 +0530

    Update Page6Sub2.tsx

--- src/Pages_main/Pages_sub6/Page6Sub2.tsx
+++ src/Pages_main/Pages_sub6/Page6Sub2.tsx
@@ -5,12 +5,7 @@ const Page6Sub2: React.FC = () => {
   return (
     <div className="pages_sub1">
       <div className="content-container">
-        <h1>Kirchhayn Bungalow</h1>
-        <p>Kirchhayn Bungalow, located in the picturesque town of Bandarawela, Sri Lanka, is a charming colonial-era retreat that showcases the island's rich history and natural beauty.
-          Originally built as a residence for a European tea planter during the British colonial period, it has been beautifully restored into a boutique hotel that combines old-world charm with modern comforts.
-          Guests are welcomed by elegant colonial architecture, complete with whitewashed walls and spacious verandas surrounded by manicured gardens. Inside, the bungalow features high ceilings,
-          polished wooden floors, and antique furnishings, creating a serene ambiance for relaxation.Visitors can also enjoy various activities, such as guided hikes through tea plantations and nature trails,
-          or leisurely picnics by sparkling streams, all set against the stunning backdrop of Sri Lanka’s central highlands.</p>
+        <h1>Page6 Sub2</h1>
       </div>
     </div>
   );
