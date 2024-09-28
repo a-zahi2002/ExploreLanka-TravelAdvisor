a1719263ac5bc335287cc6242bc02e08227beadf
Najla200 <najlahamza52@gmail.com>
2024-09-28 11:39:48 +0530

    Update Page6Sub3.tsx

--- src/Pages_main/Pages_sub6/Page6Sub3.tsx
+++ src/Pages_main/Pages_sub6/Page6Sub3.tsx
@@ -5,16 +5,7 @@ const Page6Sub3: React.FC = () => {
   return (
     <div className="pages_sub1">
       <div className="content-container">
-        <h1>Bambarakanda Falls</h1>
-        <p>Bambarakanda Falls is a spectacular natural wonder nestled amidst the lush forests and rugged terrain of Sri Lanka's central highlands.
-          As the tallest waterfall in Sri Lanka, Bambarakanda captivates visitors with its majestic beauty and awe-inspiring cascade, making it a must-visit destination for nature lovers and adventurers alike.
-Located near the town of Haputale in the Badulla District, Bambarakanda Falls tumbles dramatically down a sheer cliff face,
-          plunging approximately 263 meters (863 feet) into a rocky pool below. The sight of the cascading water, framed by verdant vegetation and misty spray, creates a mesmerizing spectacle that leaves visitors spellbound.To reach Bambarakanda Falls,
-          visitors travel along scenic winding roads through lush valleys, tea plantations, and dense forests, catching glimpses of the waterfall along the way. Upon arrival, they are greeted by the thunderous roar and cool mist of the falls,
-          creating a refreshing oasis.
-          The viewing platform provides stunning views of the cascading water, while the rocky pool below invites adventurous guests for a dip. Surrounded by pristine wilderness, the area offers numerous hiking trails that lead to breathtaking viewpoints,
-          perfect for capturing memorable photos. For a more leisurely experience, visitors can enjoy picnics amidst the serene beauty of the falls, soaking in the tranquil ambiance of nature.
-</p>
+        <h1>Page6 Sub3</h1>
       </div>
     </div>
   );
