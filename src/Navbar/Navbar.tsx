4913516a95a7c86a3c044812328d083a1593ad69
MayooriEkanthan <mayooriekanthan90@gmail.com>
2024-10-22 00:12:57 +0530

    mm

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,19 +1,29 @@
-import './Navbar.css';
+import "./Navbar.css";
 
 const Navbar = () => {
-  return (
-    <header>
-      <nav className='nav'>
-      <a href ="#" className="logo"><img src="./Long Logo.png" alt="logo"></img></a>
-        <ul className='nav-menu'>
-          <li><a href='#'>Home</a></li>
-          <li><a href='#'>Explore</a></li>
-          <li><a href='#'>About</a></li>
-          <li className='nav-contact'><a href='#'>Contact</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return(
+        <header>
+            <nav className='nav'>
+                <a href="#" className="logo">
+                    <img src="./Long Logo.png" alt="logo"></img>
+                </a>
+                <ul className='nav-menu'>
+                    <li>
+                        <a href='#'>Home</a>
+                    </li>
+                    <li>
+                        <a href='#'>Explore</a>
+                    </li>
+                    <li>
+                        <a href='#'>About</a>
+                    </li>
+                    <li className='nav-contact'>
+                        <a href='#'>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Navbar;
