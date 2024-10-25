5c1bc8c1871b5598d83ea4ffd1ac559336bb379f
FengZhi <zahi.mfa.2002@gmail.com>
2024-10-25 15:09:24 +0530

    nav

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,30 +1,30 @@
 import "./Navbar.css";
-import logo from "./long_logo.jpg";
+import logo from "./long_logo.png";
 
 const Navbar = () => {
-    return (
-        <header>
-            <nav className='nav'>
-                <a href="#" className="logo">
-                    <img src="{logo}" alt="logo"></img>
-                </a>
-                <ul className='nav-menu'>
-                    <li>
-                        <a href='#'>Home</a>
-                    </li>
-                    <li>
-                        <a href='#'>Explore</a>
-                    </li>
-                    <li>
-                        <a href='#'>About</a>
-                    </li>
-                    <li className='nav-contact'>
-                        <a href='#'>Contact</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
-}
+  return (
+    <header>
+      <nav className="nav">
+        <a href="#" className="logo">
+          <img src={logo} alt="logo"></img>
+        </a>
+        <ul className="nav-menu">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Explore</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li className="nav-contact">
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
