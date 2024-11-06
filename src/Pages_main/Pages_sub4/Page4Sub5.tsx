639fd46f70fc96fbbcf6dd438fee93bf8487f9db
Najla200 <najlahamza52@gmail.com>
2024-11-06 11:35:09 +0530

    Pilkington point edited

--- src/Pages_main/Pages_sub4/Page4Sub5.tsx
+++ src/Pages_main/Pages_sub4/Page4Sub5.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import "../Pages_sub1.css";
+import Pilkington1 from "/src/uva img/pilkington1.png";
+import Pilkington2 from "/src/uva img/pilkington2.png";
+import Pilkington3 from "/src/uva img/pilkington3.png";
 
 const Page4Sub5: React.FC = () => {
+  const handleClose = () => {
+    console.log("Page closed");
+    window.history.back();
+  };
   return (
-    <div className="pages_sub1">
+    <div className="subpage-container">
       <div className="content-container">
+      <button className="close-button" onClick={handleClose}>
+          ✕
+        </button>
         <h1>Pilkington Point</h1>
+        <div className="image-section">
+          <img src={Pilkington1} alt="Pilkington Point  View 01" />
+          <img src={Pilkington2} alt="Pilkington Point  View 02" />
+          <img src={Pilkington3} alt="Pilkington Point  View 03" />
+        </div>
+        <div className="lower-section">
+        <div className="description">
         <p>
         Pilkington Point is situated near Poonagala, which is a small town located in the central highlands of Sri 
 Lanka, near Haputale in the Badulla District. Pilkington Point offers stunning panoramic views of the 
@@ -21,6 +38,31 @@ Apart from enjoying the scenic views, visitors to Pilkington Point can engage in
 walks, birdwatching, and picnics amidst the serene surroundings. The tranquil ambiance of the viewpoint 
 makes it an ideal spot for relaxation and rejuvenation.        
         </p>
+      </div>
+      <div className="additional-details">
+            <h3>Travel Information</h3>
+            <ul>
+              <li>Location: Bandarawela, Sri Lanka</li>
+              <li>Best Time to Visit: January to April</li>
+              <li>
+                <a
+                  href="https://maps.app.goo.gl/6TEBSixmgjxwzLmz8"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View on Google Maps
+                </a>
+              </li>
+            </ul>
+            <h3>Nearby Attractions</h3>
+            <ul>
+              <li>Thangamale Bird Sanctuary </li>
+              <li>Adisham Bungalow </li>
+              <li>Dambatenne Tea Factory   </li>
+              
+            </ul>
+          </div>
+    </div>
       </div>
     </div>
   );
