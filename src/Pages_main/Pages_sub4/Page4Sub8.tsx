029778070bee8bac0076bfb1c5127c9ac83fcf95
Najla200 <najlahamza52@gmail.com>
2024-11-06 16:27:29 +0530

    Nildiya pokuna page edited

--- src/Pages_main/Pages_sub4/Page4Sub8.tsx
+++ src/Pages_main/Pages_sub4/Page4Sub8.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import "../Pages_sub1.css";
+import Pokuna1 from "/src/uva img/pokuna1.png";
+import Pokuna2 from "/src/uva img/pokuna2.png";
+import Pokuna3 from "/src/uva img/pokuna3.png";
 
 const Page4Sub8: React.FC = () => {
+  const handleClose = () => {
+    
+    console.log("Page closed");
+    window.history.back(); 
+  };
   return (
-    <div className="pages_sub1">
+    <div className="subpage-container">
+       {" "}
       <div className="content-container">
+      <button className="close-button" onClick={handleClose}>
+          ✕
+        </button>
         <h1>Nil Diya Pokuna</h1>
+        <div className="image-section">
+          <img src={Pokuna1} alt="Nildiya Pokuna View 1" />
+          <img src={Pokuna2} alt="Nildiya Pokuna View 2"/>
+          <img src={Pokuna3} alt="Nildiya Pokuna View 3"/>
+        </div>
+        <div className="lower-section">
+        <div className="description">
         <p>
         Nil Diya Pokuna, also known as the "Blue Water Pond," is a hidden gem located near the town of Ella in Sri Lanka's central highlands. 
         This enchanting natural pool, nestled among lush greenery and surrounded by rocky cliffs, offers a serene escape for nature lovers and 
@@ -17,6 +36,30 @@ const Page4Sub8: React.FC = () => {
           or late afternoon when the sunlight dances on the water, casting a magical glow over this idyllic setting.
     
         </p>
+      </div>
+      <div className="additional-details">
+            <h3>Travel Information</h3>
+            <ul>
+              <li>Location: Udakirida, Sri Lanka</li>
+              <li>Best Time to Visit:December to March</li>
+              <li>
+                <a
+                  href="https://maps.app.goo.gl/PPxSHrmeQ17AUYPE6"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View on Google Maps
+                </a>
+              </li>
+            </ul>
+            <h3>Nearby Attractions</h3>
+            <ul>
+              <li>Nine Arches Bridge</li>
+              <li> Diyaluma Falls</li>
+              <li>Ella Rock</li>
+            </ul>
+          </div>
+    </div>
       </div>
     </div>
   );
